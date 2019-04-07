5cabf3dd00307cac57f993c1ba16a4a695b5720e
Bramandityo <bramandityo@maksiplus.com>
2019-04-07 17:48:46 +0700

    fort

--- newapp/utils.py
+++ newapp/utils.py
@@ -3,6 +3,9 @@ import os
 def get_app_template_dir():
     return os.path.dirname(__file__) + "/app_templates"
 
+def get_app_template_path(name):
+    return os.path.dirname(__file__) + "/app_templates/" + name
+
 def get_app_templates():
     data = []
 
